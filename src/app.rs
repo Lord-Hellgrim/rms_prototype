1cbdfcdbfc9c2dd12442d774c370ec66fd669a64
lord-hellgrim <hallgrimurv@gmail.com>
2024-01-05 13:15:18 +0000

    just a backup

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,8 @@
+use std::time::Duration;
+
 use egui::RichText;
+use poll_promise::Promise;
+use EZDB;
 
 use crate::components;
 
@@ -40,6 +44,12 @@ impl Default for LoginScreen {
     }
 }
 
+#[derive(Default)]
+pub struct AdminScreen {
+    pub text: String,
+    promise: Option<poll_promise::Promise<String>>,
+}
+
 // ##################### THIS IS THE STATE OF THE APPLICATION ###################################################
 pub struct App {
     pub label: String,
@@ -47,6 +57,7 @@ pub struct App {
     pub value: f32,
     pub lines: Vec<Product>,
     pub login_screen: LoginScreen,
+    pub admin_screen: AdminScreen,
 }
 
 impl Default for App {
@@ -58,6 +69,7 @@ impl Default for App {
             value: 2.7,
             lines: Vec::new(),
             login_screen: LoginScreen::default(),
+            admin_screen: AdminScreen::default(),
         }
     }
 }
@@ -93,35 +105,20 @@ impl eframe::App for App {
 
 
         match self.screen {
-            Screen::Admin => show_default_layout(self, ctx),
-            Screen::Login => show_login_layout(self, ctx),
-            Screen::Purchase => show_default_layout(self, ctx),
-            Screen::Transfer => show_default_layout(self, ctx),
-            Screen::Sales => show_default_layout(self, ctx),
+            Screen::Admin => show_admin_screen(self, ctx),
+            Screen::Login => show_login_screen(self, ctx),
+            Screen::Purchase => show_default_screen(self, ctx),
+            Screen::Transfer => show_default_screen(self, ctx),
+            Screen::Sales => show_default_screen(self, ctx),
         };
 
     }
 }
 
+// ################# LOGIN SCREEN #############################################################################
 
-
-
-pub fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
-
-pub fn show_login_layout(app: &mut App, ctx: &egui::Context) {
-    components::default_top_bar(ctx, app);
+pub fn show_login_screen(app: &mut App, ctx: &egui::Context) {
+    // components::default_top_bar(ctx, app);
 
     egui::CentralPanel::default().show(ctx, |ui| {
         ui.vertical(|ui| {
@@ -147,23 +144,84 @@ pub fn show_login_layout(app: &mut App, ctx: &egui::Context) {
 
     });
 
-    // if ctx.input(|i| i.key_pressed(egui::Key::Enter)) {
-    //     if app.login_screen.password == "admin" && app.login_screen.username == "admin" {
-    //         app.login_screen = LoginScreen::default();
-    //         app.screen = Screen::Admin;
-    //     } else {
-    //         app.login_screen.error = "Wrong username or password".to_owned();
-    //     } 
+    if ctx.input(|i| i.key_pressed(egui::Key::Enter)) {
+        if app.login_screen.password == "admin" && app.login_screen.username == "admin" {
+            app.login_screen = LoginScreen::default();
+            app.screen = Screen::Admin;
+        } else {
+            app.login_screen.error = "Wrong username or password".to_owned();
+        } 
         
-    // }
+    }
+}
+
+// ########################### END OF LOGIN SCREEN ##################################################
+
+pub fn show_admin_screen(mut app: &mut App, ctx: &egui::Context) {
+    components::default_top_bar(ctx, app);
+
+    
+    egui::CentralPanel::default().show(ctx, |ui| {
+        let ctx_clone = ctx.clone();
+        if ui.button("Test poll_promise").clicked() {
+            let promise = Promise::spawn_thread("test", move || {
+                let good_csv = EZDB::client_networking::download_table("127.0.0.1:3004", "admin", "admin", "good_csv");
+                ctx_clone.request_repaint(); // wake up UI thread
+                match good_csv {
+                    Ok(csv) => csv,
+                    Err(e) => format!("Could not retreive data because: {e}"),
+                }
+            });
+            app.admin_screen.promise = Some(promise);
+        }
+
+        if let Some(promise) = &app.admin_screen.promise {
+            if let Some(text) = promise.ready() {
+                app.admin_screen.text = text.clone();
+            }
+        }
+
+        ui.heading("ADMINISTRATION");
+        
+        ui.horizontal(|ui| {
+            let t = app.admin_screen.text.clone();
+            ui.label(t.to_string());
+            ui.text_edit_singleline(&mut app.label);
+        });
+        
+        ui.add(egui::Slider::new(&mut app.value, 0.0..=10.0).text("value"));
+        if ui.button("Increment").clicked() {
+            app.value += 1.0;
+        }
+
+        ui.separator();
+        
+        ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+            powered_by_egui_and_eframe(ui);
+            egui::warn_if_debug_build(ui);
+        });
+    });
 }
 
 
 
 
-pub fn show_default_layout(mut app: &mut App, ctx: &egui::Context) {
+pub fn show_default_screen(mut app: &mut App, ctx: &egui::Context) {
     components::default_top_bar(ctx, &mut app);
     
     components::default_central_panel(app, ctx);
     
+}
+pub fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
 }
