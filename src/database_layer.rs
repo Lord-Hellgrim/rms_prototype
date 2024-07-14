bc344914673a8970136f1eac5a4f5383fd662cfb
Hallgrímur Eggertsson <hallgrimurv@gmail.com>
2024-07-14 21:32:03 +0000

    backing up

--- src/database_layer.rs
+++ src/database_layer.rs
@@ -1,87 +1,57 @@
-use mysql::*;
-use mysql::prelude::*;
-
-#[derive(Debug, PartialEq, Eq)]
-struct Payment {
-    customer_id: i32,
-    amount: i32,
-    account_name: Option<String>,
-}
+use duckdb::{params, Connection, Result};
+use duckdb::arrow::record_batch::RecordBatch;
+use duckdb::arrow::util::pretty::print_batches;
 
 #[derive(Debug)]
-struct Product {
+struct Person {
     id: i32,
-    price: f32,
     name: String,
-    description: String,
-    picture: String,
+    data: Option<Vec<u8>>,
 }
 
-
 #[cfg(test)]
-mod tests {
+mod tests{
 
     use super::*;
 
     #[test]
-    fn test_mysql() {
-        let url = "mysql://eztester:eztest@localhost:3306/ezdbtest";
-        let pool = Pool::new(url).unwrap();
+    fn test_duckdb() -> Result<()> {
+        let conn = Connection::open_in_memory()?;
 
-        let mut conn = pool.get_conn().unwrap();
+        conn.execute_batch(
+            r"CREATE SEQUENCE seq;
+            CREATE TABLE person (
+                    id              INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq'),
+                    name            TEXT NOT NULL,
+                    data            BLOB
+                    );
+            ")?;
+        let me = Person {
+            id: 0,
+            name: "Steven".to_string(),
+            data: None,
+        };
+        conn.execute(
+            "INSERT INTO person (name, data) VALUES (?, ?)",
+            params![me.name, me.data],
+        )?;
 
-        let result = conn.query_map(
-                    "SELECT * from products",
-                    |(id, price, name, description, picture)| {
-                        Product {id, price, name, description, picture}
-                    },
-                ).unwrap();
-
-        println!("{:?}", result);
-        // Let's create a table for payments.
-        conn.query_drop(
-            r"CREATE TABLE payment (
-                customer_id int not null,
-                amount int not null,
-                account_name text
-            )").unwrap();
-
-        let payments = vec![
-            Payment { customer_id: 1, amount: 2, account_name: None },
-            Payment { customer_id: 3, amount: 4, account_name: Some("foo".into()) },
-            Payment { customer_id: 5, amount: 6, account_name: None },
-            Payment { customer_id: 7, amount: 8, account_name: None },
-            Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
-        ];
-
-        // Now let's insert payments to the database
-        conn.exec_batch(
-            r"INSERT INTO payment (customer_id, amount, account_name)
-            VALUES (:customer_id, :amount, :account_name)",
-            payments.iter().map(|p| params! {
-                "customer_id" => p.customer_id,
-                "amount" => p.amount,
-                "account_name" => &p.account_name,
+        let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+        let person_iter = stmt.query_map([], |row| {
+            Ok(Person {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                data: row.get(2)?,
             })
-        ).unwrap();
+        })?;
 
-        // Let's select payments from database. Type inference should do the trick here.
-        let selected_payments = conn
-            .query_map(
-                "SELECT customer_id, amount, account_name from payment",
-                |(customer_id, amount, account_name)| {
-                    Payment { customer_id, amount, account_name }
-                },
-            ).unwrap();
+        for person in person_iter {
+            println!("Found person {:?}", person.unwrap());
+        }
 
-        // Let's make sure, that `payments` equals to `selected_payments`.
-        // Mysql gives no guaranties on order of returned rows
-        // without `ORDER BY`, so assume we are lucky.
-        assert_eq!(payments, selected_payments);
-        println!("Yay!");
-
-        conn.query_drop(
-            r"DROP TABLE payment").unwrap();
-
+        // query table by arrow
+        let rbs: Vec<RecordBatch> = stmt.query_arrow([])?.collect();
+        print_batches(&rbs);
+        Ok(())
     }
 }
